188a2598576eec24cfb0cd4af3fd0c658f011337
myskam7 <mksmflppv98@gmail.com>
2018-02-08 12:04:35 -0500

    app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       cons = require('consolidate'),
       dust = require('dustjs-helpers'),
-      pg = require('pg'),
+      { Pool, Client } = require('pg'),
       app = express();
 
-
+    const PORT = 3000; 
 
       //DB Connect String
-      const connect = 'postgresql://maxim:qvez21@database.server.com:3211/recipebookdb';
+      const connectionString = 'postgresql://maxim:qvez21@database.server.com:3211/recipebookdb';
 
 
       //Assign Dust Engine To .dust Files
@@ -27,3 +27,36 @@ const express = require('express'),
       //Body Parser Middleware
       app.use(bodyParser.urlencoded({extended: false}));
       app.use(bodyParser.json());
+
+      app.get('/', (req, res) => {
+        
+        
+         //PG Connect
+         const pool = new Pool({
+            connectionString: connectionString,
+          })
+          
+          pool.query('SELECT NOW()', (err, res) => {
+            console.log(err, res)
+            pool.end()
+          })
+          
+          const client = new Client({
+            connectionString: connectionString,
+          })
+          client.connect()
+          
+          client.query('SELECT * FROM recipes', (err, res) => {
+            // console.log(err, res)
+            if(err){
+                return console.error('error running query', err);
+            }
+            res.render('index', {recipes: result.rows});
+            client.end()
+          })
+      });
+
+      //Server
+    app.listen(PORT, () => {
+        console.log(('Server Started on Port ' + PORT));
+    })
