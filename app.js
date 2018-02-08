392757a03909d2d32034e85f226627a3a3fab4ba
myskam7 <mksmflppv98@gmail.com>
2018-02-08 10:54:32 -0500

    app.js

--- app.js
+++ app.js
@@ -14,3 +14,16 @@ const express = require('express'),
 
       //Assign Dust Engine To .dust Files
       app.engine('dust', cons.dust);
+     
+
+      //Set Default Ext .dust
+      app.set('view engine','dust');
+      app.set('views', __dirname + '/views');
+
+      //Set Public Folder
+      app.use(express.static(path.join(__dirname, 'public')));
+
+
+      //Body Parser Middleware
+      app.use(bodyParser.urlencoded({extended: false}));
+      app.use(bodyParser.json());
